8022d3253329a3e47c447fb30f97259469c85fb2
Nguyen Nhan <nhanb2014679@student.ctu.edu.vn>
2023-11-04 15:34:44 +0700

    Thêm hỗ trợ và liên hệ

--- general/nav.php
+++ general/nav.php
@@ -1,8 +1,8 @@
  <nav class="main-menu nav row my-3">
     <ul class="menu col-sm-9">
         <li><a href="index.php">Trang Chủ</a></li>              
-        <li><a href="">Hỗ trợ</a></li>
-        <li><a href="">Liên hệ</a></li>
+        <li><a href="support.php">Hỗ trợ</a></li>
+        <li><a href="contact.php">Liên hệ</a></li>
     </ul>
     <form class="d-flex col-sm justify-content-end search-bar" action="search.php" method="post">
         <input class="form-control me-2" type="text" placeholder="Tìm kiếm sách bạn muốn...." name="search">
---
+++ public/support.php
@@ -0,0 +1,95 @@
+<?php
+
+define('TITLE', 'Hỗ trợ');
+include_once __DIR__ . '/../general/header.php';
+require_once __DIR__ . '/../general/connect.php';
+
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!isset($_SESSION['user']))
+    {
+        $_SESSION['msg'] = 'Bạn cần phải đăng nhập để có thể gửi ý kiến đến chúng tôi.';
+        header("Location: login.php");
+        
+        exit();
+    }
+
+    $query = 'insert into binhluan values (?,?,?)';
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute(['', $_SESSION['user'], $_POST['comment']]);
+
+    $_SESSION['msg'] = 'Chúng tôi đã ghi nhận ý kiên của bạn.';
+    header('Location: support.php');
+}
+
+include_once __DIR__ . '/../general/nav.php';
+
+?>
+<h1 class="text-center">HỖ TRỢ KHÁCH HÀNG</h1>
+<hr>
+
+<div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
+        1. Tôi có thể trả tiền thông qua PayPal?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <strong>Được. Chúng tôi chấp nhận việc chi trả qua PayPal.  </strong>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+        2. Tôi có thể hủy bỏ đặt hàng?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <strong>Bạn có thể hủy bỏ đơn hàng nếu đơn hàng đó chưa được xác nhận.</strong> 
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+        3. Chính sách hoàn tiền như thế nào?  
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <strong>Bạn có thể nhận được tiền hoàn trả 100% bất kể khi nào và vì bất kỳ lý do gì.</strong>
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+        4. Làm thế nào để tôi có thể nhận được hóa đơn?
+      </button>
+    </h2>
+    <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse">
+      <div class="accordion-body">
+        <strong>Bạn có thể nhận được hóa đơn thông qua email hoặc thông báo từ chúng tôi.</strong>
+      </div>
+    </div>
+  </div>
+</div>
+<form method="POST">
+        
+    <div class="form-group my-2">
+        <label for="comment">Câu hỏi khác:</label>
+        <textarea class="form-control" placeholder="Hãy viết nội dung bạn cần hỏi ở đây." name="comment" id="comment" style="height: 100px"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary mt-2">
+        Gửi
+    </button>
+</form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
