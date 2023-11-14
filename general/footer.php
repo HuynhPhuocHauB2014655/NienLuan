dfc4ac5b63929d4483091935f3f9528c243f8022
HuynhPhuocHauB2014655 <haub2014655@student.ctu.edu.vn>
2023-11-14 17:45:23 +0700

    14/11/2023

--- general/footer.php
+++ general/footer.php
@@ -1,7 +1,12 @@
  
 </div>
-<footer class="container footer mt-auto text-center">
-        <p>Copyright &copy; 2023 Web Development Course</p>
+<footer class="d-flex justify-content-between container footer mt-auto align-item-center">
+        <p class="mb-0 py-3 text-white">H&NSHOP &copy; </p>
+        <p class="mb-0 py-3 d-flex">
+            <a href="#" class="nav-link link-white"><i class="fa-brands fa-facebook me-2 text-white"></i></a>
+            <a href="#" class="nav-link link-white"><i class="fa-brands fa-youtube me-2 text-white"></i></a>
+            <a href="#" class="nav-link link-white"> <i class="fa-brands fa-twitter me-2 text-white"></i></a>
+        </p>
     </footer>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
