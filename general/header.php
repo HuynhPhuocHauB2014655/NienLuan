8b58735e1afad64939ee45802ae3d0d97142e033
HuynhPhuocHauB2014655 <haub2014655@student.ctu.edu.vn>
2023-12-01 09:48:28 +0700

    add shortcut icon

--- general/header.php
+++ general/header.php
@@ -57,7 +57,7 @@ require_once __DIR__ . '/../general/connect.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style copy.css">
-    <link rel="shortcut icon" type="image/jpg" href="">
+    <link rel="shortcut icon" type="image/jpg" href="images/LOGO-2.png">
     <title><?php
             if (defined('TITLE')) {
                 echo TITLE;
